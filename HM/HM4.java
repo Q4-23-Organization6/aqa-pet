eed16505b6b5b4221751fb2be4bef771576c0c5f
marikhors <mkhorsun13@gmail.com>
2023-12-21 12:50:24 +0200

    newone

---
+++ HM/HM4.java
@@ -0,0 +1,19 @@
+public class HM4 {
+        public static void main(String[] args) {
+            // String b = "Hello world!";
+            // System.out.println(b);
+
+        /* int Fahrenheit = 50;
+        //double Celsius = (Fahrenheit - 32) * 5.0 / 9;
+        double Kelvin = Celsius + 273.16;
+        System.out.println(Fahrenheit + " Fahrenheit");
+        System.out.println(Celsius + " Celsius");
+        System.out.println(Kelvin + " Kelvin"); */
+
+            int a = 3;
+            int b = 4;
+            int S = (a * b) / 2;
+            System.out.println(S);
+        }
+    }
+
