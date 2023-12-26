14b1334e38de132685c244ed28c8503290a21c7d
marikhors <mkhorsun13@gmail.com>
2023-12-26 12:39:21 +0200

    HW 6

---
+++ HW6/CheckOddEven.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CheckOddEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("Even number");
+        } else {
+            System.out.println("Odd number");
+        }
+
+        System.out.println("BYE");
+
+    }
+}
